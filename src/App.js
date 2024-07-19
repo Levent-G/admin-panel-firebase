86bd736378b808249aee35d269fad8ffde054960
Levent-G <aquariiv@outlook.com>
2024-07-19 20:14:08 +0300

    kütüphaneler kuruldu..

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-;
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminPanelMain from './pages/AdminPanelMain';
 
 function App() {
   return (
     <div className="App">
-     
+       <Router>
+            <Routes>
+                <Route path="/" element={<AdminPanelMain />} />
+             
+            </Routes>
+        </Router>
     </div>
   );
 }
---
+++ src/pages/AdminPanelMain.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminPanelMain = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdminPanelMain
